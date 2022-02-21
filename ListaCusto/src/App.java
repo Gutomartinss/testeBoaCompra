3dbd4d75dc14926a14ee08dee07dec6f086344e1
Gustavo Martins <gutoh1@hotmail.com>
2022-02-21 13:53:35 -0300

    Update App.java
    Funcionando apenas para a Transboa. Corrigindo para as demais.

--- ListaCusto/src/App.java
+++ ListaCusto/src/App.java
@@ -13,11 +13,12 @@ public class App {
         Produto controleXbox1 = new Produto("ControleXbox",1,3);
         Produto controleXbox2 = new Produto("ControleXbox",100,3);
         Produto pcGamer = new Produto("PC_Gamer",1000,35);
+        Produto pcGamer2 = new Produto("PC_Gamer",1,35);
         Produto kitGamer = new Produto("Kit_Gamer",1000,5); 
         Produto tecladoMaisFone = new Produto("TecladoMaisFone",5,6);
 
         Produto [] arrayDeProdutos = {foneDeOuvido1,foneDeOuvido2,foneDeOuvido3,foneDeOuvido4,controleXbox1,
-        controleXbox2,pcGamer,kitGamer,tecladoMaisFone};
+        controleXbox2,pcGamer,pcGamer2, kitGamer,tecladoMaisFone};
 
         Empresa boaDex = new Empresa("BoaDex",10,0.05);
         Empresa boaLog = new Empresa("BoaLog",4.30,0.12);
@@ -26,6 +27,8 @@ public class App {
         Empresa [] arrayDeEmpresa = {boaDex,boaLog,transBoa};
 
         Custo [] arrayDeCustos = new Custo[30];
+
+        inicializaArrayDeCustos(arrayDeCustos, 30);
         
 
         calculaCusto(arrayDeCustos,arrayDeProdutos, arrayDeEmpresa);
@@ -44,14 +47,14 @@ public class App {
          
           for(int i=0;i<10;i++){
             
-            arrayDeCustos[i].custoEnvio = arrayDeTransportadora[contadorEmpresa].valor_Fixo +(arrayDeBrinde[i].peso*
+            arrayDeCustos[i].custoEnvio = arrayDeTransportadora[contadorEmpresa].valor_Fixo+(arrayDeBrinde[i].peso*
             arrayDeBrinde[i].distancia*arrayDeTransportadora[contadorEmpresa].valorKgKm);
 
             arrayDeCustos[i].nomeDoProduto = arrayDeBrinde[i].nome;
-            arrayDeCustos[i].nomeDaTransportadora = arrayDeTransportadora[i].nome;
+            arrayDeCustos[i].nomeDaTransportadora = arrayDeTransportadora[contadorEmpresa].nome;
 
           }
-          contadorEmpresa++;
+          contadorEmpresa=contadorEmpresa+1;
 
         }
 
@@ -61,7 +64,13 @@ public class App {
 
     public static void listaCustos(Custo[] arrayDeCustos){
         for(int i=0;i<30;i++){
-            System.out.println("Produto: " + arrayDeCustos[i].nomeDoProduto);
+            System.out.println("Produto: " + arrayDeCustos[i].nomeDoProduto+" Empresa: "+arrayDeCustos[i].nomeDaTransportadora);
+        }
+    }
+
+    public static void inicializaArrayDeCustos(Custo[] arrayDeCustos, double tamanhoArray){
+        for(int i=0;i<tamanhoArray;i++){
+            arrayDeCustos[i] = new Custo();
         }
     }
 
